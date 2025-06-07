4811a93b84b8bf1d6593e4ea01a19bec4e05fe93
Swayamsiuuuuu7 <swayama764@gmail.com>
2025-06-07 21:23:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,5 +9,5 @@ app.use(express.json());
 
 app.use('/api', forecastRoute);
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
